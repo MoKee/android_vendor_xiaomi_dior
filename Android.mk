06eac79e2458c08c82b622a14ad78723e351d0c0
subdragonzj <zj502407679@gmail.com>
2018-01-22 13:35:07 +0800

    Revert "dior: use prebuilt GoogleCamera"
    This reverts commit f58095b5064cf6d4616865f800cd1680c607f5dd.
    
    Change-Id: I15d2039d0bd9099026c4da237a08c98afb98cffa

--- Android.mk
+++ Android.mk
@@ -60,16 +60,6 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := GoogleCamera
-LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/app/GoogleCamera/GoogleCamera.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.qti.hardware.fm@1.0
 LOCAL_MODULE_OWNER := xiaomi
 LOCAL_SRC_FILES := proprietary/lib/vendor.qti.hardware.fm@1.0.so
--- dior-vendor.mk
+++ dior-vendor.mk
@@ -16,8 +16,7 @@
 
 # Prebuilt APKs
 PRODUCT_PACKAGES += \
-    TimeService \
-    GoogleCamera
+    TimeService
 
 # Prebuilt vendor/libs needed for compilation
 PRODUCT_PACKAGES += \
